18835ff795b83c0c5e1db2a0d39889e58d96e11d
Modesire Soneye <67333381+Desireye@users.noreply.github.com>
2023-06-13 18:39:51 -1100

    css changes and added js

---
+++ fylo-land.js
@@ -0,0 +1,7 @@
+const getStartedBtn = document.getElementById("get-started-btn")
+const checkEl = document.getElementById("check-el")
+const emailInput = document.getElementById("email-input")
+
+getStartedBtn.addEventListener("click", function() {
+    console.log(getStartedBtn, checkEl, emailInput)
+})
