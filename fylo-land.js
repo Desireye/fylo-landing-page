5ef564019cb8d27d448bff633750f4d357a4beef
Modesire Soneye <67333381+Desireye@users.noreply.github.com>
2023-06-13 19:00:01 -1100

    css changes and js

--- fylo-land.js
+++ fylo-land.js
@@ -2,6 +2,14 @@ const getStartedBtn = document.getElementById("get-started-btn")
 const checkEl = document.getElementById("check-el")
 const emailInput = document.getElementById("email-input")
 
+const getStartedBtn2 = document.getElementById("get-started-btn-2")
+const checkEl2 = document.getElementById("check-el-2")
+const emailInput2 = document.getElementById("email-input-2")
+
 getStartedBtn.addEventListener("click", function() {
-    console.log(getStartedBtn, checkEl, emailInput)
+    checkEl.textContent = "Please check your e-mail"
+})
+
+getStartedBtn2.addEventListener("click", function() {
+    checkEl2.textContent = "Please check your e-mail"
 })
